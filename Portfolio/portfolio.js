793f14d44497763c32462ed30ec8d1f51f1aed75
Wandering-Wind <amy0071205@gmail.com>
2025-05-22 19:19:34 +0200

    Portfolio js buttons

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,32 @@
+
+// List of blog pages in order
+const blogPages = [
+  "game_analysis_1.html"
+];
+
+// Get current page filename
+const currentPage = window.location.pathname.split("/").pop();
+
+// Find current index in the blogPages array
+const currentIndex = blogPages.indexOf(currentPage);
+
+// Get buttons
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+// Enable/disable and set up navigation
+if (prevBtn && currentIndex > 0) {
+  prevBtn.onclick = () => {
+    window.location.href = blogPages[currentIndex - 1];
+  };
+} else if (prevBtn) {
+  prevBtn.disabled = true;
+}
+
+if (nextBtn && currentIndex < blogPages.length - 1) {
+  nextBtn.onclick = () => {
+    window.location.href = blogPages[currentIndex + 1];
+  };
+} else if (nextBtn) {
+  nextBtn.disabled = true;
+}
